3cb0ee9a2acb6ee2b091e79abf5deb12aea03f32
unknown <gabemarredondo@gmail.com>
2022-10-25 14:34:37 -0700

    added authentication to the login page

--- client/utils/mutations.js
+++ client/utils/mutations.js
@@ -1,5 +1,17 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
 export const CREATE_NAME = gql`
   mutation createName($: String!, $: String!) {
     createMatchup() {
