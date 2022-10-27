5cf6f25de15897da7085d6c6189d8384f90c8ba6
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-10-27 14:48:47 -0700

    updayted mutations.js

---
+++ client/src/pages/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+
+import Auth from "../utils/auth";
+import LoginForm from "../components/LoginForm";
+
+const Login = (props) => {
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    try {
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
+
+      console.log(data);
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setUserFormData({
+      email: '',
+      password: '',
+    });
+  };
+
+  return (
+    <main className="flex-row justify-center mb-4">
+    <LoginForm error={error} onSubmit={handleFormSubmit} userFormData={userFormData} setUserFormData={setUserFormData}/>
+    </main>
+  );
+};
+
+export default Login;
