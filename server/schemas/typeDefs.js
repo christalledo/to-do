f9189eca888270ccbbe78aa8f798e65638d5854a
ChrisCODES23 <christiantalledo89@gmail.com>
2022-10-27 11:31:01 -0700

    fixed mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -17,7 +17,6 @@ const typeDefs = gql`
     password: String
     todo: String!
   }
-  
   type Auth {
     token: ID!
     user: User
@@ -31,9 +30,9 @@ const typeDefs = gql`
     createList(_id: ID!, assigned: String!) : List
     deleteList(_id: ID!, assigned: String! ) : List
     updateList(_id: ID!, assigned: String!) : List
-    createToDo(todo: String!) : User
-    updatedToDo(todo: String!) : User
-    deletedToDo(todo: String!) : User
+    createToDo(user_id: ID!, todo: String!) : User
+    updatedToDo(user_id: ID!, todo: String!) : User
+    deletedToDo(user_id: ID!,  todo: String!) : User
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
   }
