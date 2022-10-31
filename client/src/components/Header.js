a368772a6f90268e07890a39fb5e3e1eed0be9a2
Latifah Zawedde <zawedde2021@gmail.com>
2022-10-31 14:07:21 -0700

    create footer

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Auth from '../../utils/auth';
+import Auth from '../utils/auth';
 
 const Header = () => {
   const logout = (event) => {
@@ -9,8 +9,8 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
-      <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
+    <header className="bg-info  text-dark mb-2 py-3 display-flex align-center">
+      <div className="container flex-column justify-space-between-lg justify-center  align-center text-center">
         <Link className="text-dark" to="/">
           <h1 className="m-0" style={{ fontSize: '3rem' }}>
             TO Do
